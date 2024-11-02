09fc4988c8247c546264835388483b46082d986a
Erik Silva Maia <erikm384@gmail.com>
2024-11-01 22:49:12 -0300

    build: create project

---
+++ src/main/java/br/utfpr/agro_radar/AgroRadarApplication.java
@@ -0,0 +1,13 @@
+package br.utfpr.agro_radar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroRadarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroRadarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/utfpr/agro_radar/AgroRadarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.utfpr.agro_radar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroRadarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
